c0dae4690de81f09eeb660e27fa259e9202e7993
Angie <angie.godinezv@gmail.com>
2019-09-10 13:56:08 -0700

    Modified main anova and added anova analysis for tc, ppr, and asymptote

--- analysis/ppr_tc_asymptote_ANOVA.py
+++ analysis/ppr_tc_asymptote_ANOVA.py
@@ -1,3 +1,5 @@
+# 2x3 way between subjects ANOVA on TC, Asymptote, and PPR
+
 import pandas as pd
 import os
 import statsmodels.api as sm
@@ -14,19 +16,18 @@ obs_set = {'ah', 'aj', 'dd', 'dl', 'ez', 'it', 'll', 'sh', 'sm', 'sr', 'bb', 'by
 
 # Load variables
 os.chdir('C:/Users/angie/Git Root/StereoTraining/data')
-allData = pd.read_csv('subjectData.csv')
-
-allData = allData.ix[(allData['hit']== True) & (allData['stereoacuity'] > 50)]
+allData = pd.read_csv('boxPlotData.csv')
 
 subjects = allData.subject.unique()
 difficulties = allData.difficulty.unique()
 
 #For some reason, this gives me wrong dof..
-ols_lm = smf.ols('saLog ~ difficulty * group', data=allData)
+ols_lm = smf.ols('tc ~ difficulty * group', data=allData)
 fit = ols_lm.fit()
 table = sm.stats.anova_lm(fit, typ=2)
-print(table)
+#print(table)
 
+# Analysis for Pre to Post ratio (PPR)
 def computeP(fValue, df_diff, df_group):
     fDistribution = scipy.stats.f
     p = 1 - fDistribution.cdf(fValue, df_diff, df_group)
@@ -43,33 +44,41 @@ def omnibusTest(result):
     return (F,p)
 
 F,p = omnibusTest(table)
-print((F,p))
+#print((F,p))
+
+def geoMean(data_set):
+    data_length = len(data_set)
+    geometricMean = pow(np.prod(data_set), 1/data_length)
+    return geometricMean
 
 anovaFrame = allData.copy()
-print(anovaFrame.sample(10))
+#print(anovaFrame.sample(10))
 
 ##Hard Way
 #Calculate 2-way anova
 # DEGREES OF FREEDOM
-N = len(allData['saLog'])
+N = len(allData['tc'])
 df_diff = len(allData['difficulty'].unique()) - 1
 df_group = len(allData['group'].unique()) - 1
 df_diffxgroup = df_diff*df_group
 df_w = N - (len(allData['difficulty'].unique())*len(allData['group'].unique()))
 
-anovaFrame = allData.drop(columns=['SA[seconds] dart location', 'dichoptic errors', 'distance[m]', 'subject'])
-
 # SUMS OF SQUARES
-dataMean = anovaFrame['saLog'].mean()
-groupMeans = anovaFrame.groupby('group').saLog.mean()
-print('group means:')
-print(groupMeans)
-diffMeans = anovaFrame.groupby('difficulty').saLog.mean()
-print('Difficulty means')
-print(diffMeans)
-groupXdiffMeans = anovaFrame.groupby(['group', 'difficulty']).saLog.mean()
-print('interaction means')
-print(groupXdiffMeans)
+dataMean = anovaFrame['tc'].mean()
+groupMeans = anovaFrame.groupby('group').tc.mean()
+diffMeans = anovaFrame.groupby('difficulty').tc.mean()
+groupXdiffMeans = anovaFrame.groupby(['group', 'difficulty']).tc.mean()
+
+#Calculate geometric mean
+geoAnolMean = geoMean(allData.tc[allData['group'] == 1])
+geoNorMean = geoMean(allData.tc[allData['group'] == 0])
+
+#Calculate SEM
+semAnol = scipy.stats.sem(allData.tc[allData['group'] == 1])
+semNorm = scipy.stats.sem(allData.tc[allData['group'] == 0])
+
+print('Anomalous geometric mean: ', geoAnolMean, 'SEM: ', semAnol)
+print('Normal geometric mean: ', geoNorMean, 'SEM: ', semNorm)
 
 anovaFrame["meanEffect"] = dataMean
 group = allData.group.unique()
@@ -88,8 +97,8 @@ for g in group:
         anovaFrame.loc[selector, "interactionEffect"] = groupXdiffMeans.loc[g, diff] - anovaFrame.groupMainEffect[selector] - anovaFrame.difficultyMainEffect[selector] - dataMean
 
 # ERROR/RESIDUAL
-anovaFrame['residual'] = anovaFrame.saLog - anovaFrame.meanEffect - anovaFrame.groupMainEffect - anovaFrame.difficultyMainEffect - anovaFrame.interactionEffect
-print(anovaFrame.sample(10))
+anovaFrame['residual'] = anovaFrame.tc - anovaFrame.meanEffect - anovaFrame.groupMainEffect - anovaFrame.difficultyMainEffect - anovaFrame.interactionEffect
+#print(anovaFrame.sample(10))
 
 # SUMS OF SQUARES
 def SS(x):
@@ -97,18 +106,10 @@ def SS(x):
 
 sumofSquares = {}
 keys = ['total', 'mean', 'group', 'difficulty', 'interaction', 'residual']
-columns = [anovaFrame.saLog, anovaFrame.meanEffect, anovaFrame.groupMainEffect, anovaFrame.difficultyMainEffect, anovaFrame.interactionEffect, anovaFrame['residual']]
+columns = [anovaFrame.tc, anovaFrame.meanEffect, anovaFrame.groupMainEffect, anovaFrame.difficultyMainEffect, anovaFrame.interactionEffect, anovaFrame['residual']]
 for key, column, in zip(keys, columns):
     sumofSquares[key] = SS(column)
-print('Sums of Squares: ')
-print(sumofSquares)
-
-def geoMean(data_set):
-    data_length = len(data_set)
-    geometricMean = pow(np.prod(data_set), 1/data_length)
-    return geometricMean
-
-geoAnolMean = geoMean(anovaFrame.stereoacuity[anovaFrame.group == 'Control'])
+print('Sums of Squares: ', sumofSquares)
 
 # Calculate degrees of freedom for sums of squares and  store it into a dictionary
 dof = {}
@@ -120,8 +121,7 @@ vals = [N, 1,
 
 for key, val in zip(keys, vals):
     dof[key] = val
-print('Degrees of Freedom: ')
-print(dof)
+print('Degrees of Freedom: ', dof)
 
 # Using the dictionaries "sumsofSquares" and "dof", compute the mean square values dor all of the keyed quantities
 meanSquare = {}
@@ -134,8 +134,7 @@ print(meanSquare)
 F = {}
 for key in ['group', 'difficulty', 'interaction']:
     F[key] = meanSquare[key]/meanSquare['residual']
-print('F value: ')
-print(F)
+print('F value: ', F)
 
 #Compute p value using scipy.stats.f.cdf
 for effect in F.keys():
